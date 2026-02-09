fe70d9eaff62f95d0d0a76828cb712989cd9d599
Cristian <garciarodriguezcristian001.alu@stemgranada.com>
2026-02-10 00:22:37 +0100

    Script LLMM

---
+++ Lenguaje de marcas/Apuntes/UD5.DOM/Relacion1/Ej_1_Zoologico/js/script.js
@@ -0,0 +1,8 @@
+const animales = document.getElementsByClassName("animal")
+for (const animal of animales) {
+    animal.style.backgroundColor = "lightyellow"
+}
+const boton = document.getElementById("resaltarAnimales")
+boton.addEventListener("click", function () {
+
+})
