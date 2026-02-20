599ce905368ad3b29fa159f3da231aa7c99d095d
Cristian <garciarodriguezcristian001.alu@stemgranada.com>
2026-02-20 11:53:14 +0100

    Relacion 2 completa

--- Lenguaje de marcas/Apuntes/UD5.DOM/Relacion2/Ejercicio5_Generar_Noticias/js/script.js
+++ Lenguaje de marcas/Apuntes/UD5.DOM/Relacion2/Ejercicio5_Generar_Noticias/js/script.js
@@ -1,10 +1,8 @@
 const nuevoParrafo1 = document.createElement("p")
-nuevoParrafo1.textContent = "Descubren una nueva especie en la selva amazónica."
-
 const nuevoParrafo2 = document.createElement("p")
-nuevoParrafo2.textContent = "Avances tecnológicos revolucionan la industria de la salud."
-
 const nuevoParrafo3 = document.createElement("p")
+nuevoParrafo1.textContent = "Descubren una nueva especie en la selva amazónica."
+nuevoParrafo2.textContent = "Avances tecnológicos revolucionan la industria de la salud."
 nuevoParrafo3.textContent = "El mercado financiero experimenta grandes cambios."
 
 const contenedor = document.getElementById("contenedor")
