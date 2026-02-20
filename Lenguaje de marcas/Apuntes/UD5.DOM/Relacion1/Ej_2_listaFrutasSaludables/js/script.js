f90d758d461330fc6258001265d4495bcf5bc4b4
Cristian <garciarodriguezcristian001.alu@stemgranada.com>
2026-02-20 11:53:02 +0100

    Corrijo relacion1

--- Lenguaje de marcas/Apuntes/UD5.DOM/Relacion1/Ej_2_listaFrutasSaludables/js/script.js
+++ Lenguaje de marcas/Apuntes/UD5.DOM/Relacion1/Ej_2_listaFrutasSaludables/js/script.js
@@ -1,13 +1,15 @@
 const frutas = document.getElementsByTagName("li")
 for (const fruta of frutas) {
-    fruta.style.border = "2px solid orange"
+    fruta.style.border = "2px dotted orange"
 }
 
 const button = document.getElementById("marcarFrutas")
 button.addEventListener("click", function () {
     for (const fruta of frutas) {
+        if (!fruta.textContent.includes("✅")) {
+            fruta.textContent += "  ✅"
+        }
         fruta.style.backgroundColor = "lightblue"
-        fruta.textContent +="  ✅"
 
     }
 })
--- Lenguaje de marcas/Apuntes/UD5.DOM/Relacion1/Ej_3_PanelControlBotones/js/script.js
+++ Lenguaje de marcas/Apuntes/UD5.DOM/Relacion1/Ej_3_PanelControlBotones/js/script.js
@@ -7,8 +7,7 @@ const botones = document.querySelectorAll(".boton")
 
 botonCambiar.addEventListener("click", function () {
 for (const boton of botones) {
-    boton.style.backgroundColor = "lightgreen"
+    boton.style.backgroundColor = "lightyellow"
     boton.style.border = "solid 2px red"
 }
 })
-
